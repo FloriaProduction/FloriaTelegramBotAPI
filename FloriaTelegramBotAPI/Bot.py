7164e17479484d9c52344fe079de59481ab0fc74
FloriaProduction <FloriaProduction@yandex.ru>
2025-07-02 21:48:16 +0700

    Разделение компонентов класса Bot на WebClient и BotMethods, легкий рефакторинг

--- FloriaTelegramBotAPI/Bot.py
+++ FloriaTelegramBotAPI/Bot.py
@@ -7,27 +7,28 @@ from . import Utils, Exceptions, Enums
 from .Types import DefaultTypes, MethodForms
 from .Router import Router
 from . import Validator
+from .WebClient import WebClient
+from .BotMethods import BotMethods
 
 
 class Bot(Router):
     def __init__(self, token: str, config: Optional[Config] = None):
         super().__init__()
         
-        self._token = Validator.IsInstance(token, str)
         self._config = Validator.IsSubClass(config, Config | None) or Config()
-        self._is_enabled = True
         
-        self._client = httpx.AsyncClient(timeout=self.config.timeout)
-        self._info: Optional[DefaultTypes.User] = None
-        
-        self._update_offset = 0
+        self._client = WebClient(token, self)
+        self._methods: Optional[BotMethods] = None
         
         self._logger: Optional[logging.Logger] = None
         
-        self._methods: Optional[APIMethods] = None
+        self._info: Optional[DefaultTypes.User] = None
+        self._is_enabled = True
+        
+        self._update_offset: int = 0
         
     async def Init(self):
-        self._info = DefaultTypes.User(**(await self._RequestGet('getMe'))['result'])
+        self._info = DefaultTypes.User(**(await self._client.RequestGet('getMe'))['result'])
         
         self._logger = logging.getLogger(f'{self.info.username[:self.config.name_max_length]}{'..' if len(self.info.username) > self.config.name_max_length else ''}({self.info.id})')
         
@@ -45,16 +46,16 @@ class Bot(Router):
         self.logger.setLevel(self._config.stream_handler_level)
         self.logger.info(f'initialized')
         
-        self._methods = APIMethods(self)
+        self._methods = BotMethods(self)
     
     async def Polling(self, *, skip_updates: bool = False):
         await self.Init()
         
         if skip_updates:
-            await self._GetUpdates()
+            await self._client.GetUpdates(self._update_offset)
         
         while self.is_enabled:
-            for update in await self._GetUpdates():
+            for update in await self._client.GetUpdates(self._update_offset):
                 try:
                     self.logger.debug(f'{update=}')
                     
@@ -84,79 +85,6 @@ class Bot(Router):
                 finally:
                     pass
     
-    async def _GetUpdates(self) -> list[dict[str, Any]]:
-        return (await self._RequestGet(
-            'getUpdates', 
-            {
-                'offset': self._update_offset + 1
-            }
-        )).get('result', [])
-    
-    async def _MakeRequest(
-        self, 
-        method: Callable, 
-        command: str,
-        **kwargs
-    ) -> httpx.Response:
-        current_attempt_count = 0
-        while current_attempt_count < self.config.retry_count:
-            try:
-                response: httpx.Response = await method(
-                    url=f'https://api.telegram.org/bot{self._token}/{command}',
-                    **kwargs
-                )
-                data: dict = response.json()
-                if not response.is_success:
-                    raise Exception(f'\n\tCode: {data.get('error_code')}\n\tDescription: {data.get('description')}\n\tCommand: {command}\n\tRequest: {response.request.content}')
-                
-                return data
-                
-            except:
-                self.logger.warning('', exc_info=True)
-            
-            finally:
-                current_attempt_count += 1
-        
-        raise httpx.RequestError(f'Failed to complete request after {self.config.retry_count} attempts')
-        
-    async def _RequestGet(
-        self, 
-        command: str, 
-        data: Optional[Any] = None
-    ) -> httpx.Response:
-        return await self._MakeRequest(
-            self._client.get, 
-            command,
-            
-            params=Utils.ConvertToJson(data or {})
-        )
-    
-    async def _RequestPost(
-        self, 
-        command: str, 
-        data: Any,
-    ) -> httpx.Response:
-        return await self._MakeRequest(
-            self._client.post, 
-            command,
-            
-            json=Utils.ConvertToJson(data or {}),
-        )
-    
-    async def _RequestPostData(
-        self,
-        command: str,
-        data: Any,
-        files: Any = None
-    ) -> httpx.Response:
-        return await self._MakeRequest(
-            self._client.post,
-            command,
-            
-            data=Utils.ConvertToJson(data or {}),
-            files=files
-        )
-
     @property
     def is_enabled(self) -> bool:
         return self._is_enabled
@@ -166,6 +94,10 @@ class Bot(Router):
     @property
     def config(self) -> Config:
         return self._config
+    
+    @property
+    def client(self) -> WebClient:
+        return self._client
     
     @property
     def logger(self) -> logging.Logger:
@@ -181,102 +113,4 @@ class Bot(Router):
     def methods(self):
         if self._methods is None: raise Exceptions.NotInitializedError()
         return self._methods
-
-# API
-
-class APIMethods:
-    def __init__(self, bot: 'Bot'):
-        self._bot = Validator.IsSubClass(bot, Bot)
-    
-    @staticmethod
-    def _ResponseToMessage(response) -> DefaultTypes.Message:
-        return DefaultTypes.Message(**response['result'])
-    
-    async def SendMessage(
-        self,
-        chat_id: int,
-        text: str,
-        reply_parameters: Optional[DefaultTypes.ReplyParameters] = None,
-        reply_markup: Optional[Union[
-            DefaultTypes.InlineKeyboardMarkup,
-            DefaultTypes.ReplyKeyboardMarkup,
-            DefaultTypes.ReplyKeyboardRemove,
-            DefaultTypes.ForceReply
-        ]] = None,
-        parse_mode: Optional[str] = None,
-        business_connection_id: Optional[str] = None,
-        message_thread_id: Optional[int] = None,
-        entities: Optional[list[DefaultTypes.MessageEntity]] = None,
-        link_preview_options: Optional[DefaultTypes.LinkPreviewOptions] = None,
-        disable_notification: Optional[bool] = None,
-        protect_content: Optional[bool] = None,
-        allow_paid_broadcast: Optional[bool] = None,
-        message_effect_id: Optional[str] = None,
-        **kwargs
-    ) -> DefaultTypes.Message: 
-        kwargs.update(Utils.RemoveKeys(locals(), 'self', 'kwargs'))
-        kwargs.setdefault('parse_mode', self._bot.config.parse_mode)
-        
-        return self._ResponseToMessage(
-            await self._bot._RequestPost(
-                'sendMessage', 
-                MethodForms.SendMessage(**kwargs)
-            )
-        )
-    
-    async def SendChatAction(
-        self,
-        chat_id: str | int,
-        action: Enums.Action,
-        business_connection_id: Optional[str] = None,
-        message_thread_id: Optional[int] = None,
-        **kwargs
-    ):
-        kwargs.update(Utils.RemoveKeys(locals(), 'self', 'kwargs'))
-        await self._bot._RequestPost(
-            'sendChatAction',
-            MethodForms.SendChatAction(**kwargs)
-        )
-    
-    async def SendPhoto(
-        self,
-        chat_id: str | int,
-        photo: str | bytes,
-        caption: Optional[str] = None,
-        parse_mode: Optional[Enums.ParseMode] = None,
-        caption_entities: Optional[list[DefaultTypes.MessageEntity]] = None,
-        show_caption_above_media: Optional[bool] = None,
-        disable_notification: Optional[bool] = None,
-        protect_content: Optional[bool] = None,
-        allow_paid_broadcast: Optional[bool] = None,
-        message_effect_id: Optional[str] = None,
-        reply_parameters: Optional[DefaultTypes.ReplyParameters] = None,
-        reply_markup: Optional[Union[
-            DefaultTypes.InlineKeyboardMarkup,
-            DefaultTypes.ReplyKeyboardMarkup,
-            DefaultTypes.ReplyKeyboardRemove,
-            DefaultTypes.ForceReply
-        ]] = None,
-        business_connection_id: Optional[str] = None,
-        message_thread_id: Optional[int] = None,
-        **kwargs
-    ) -> DefaultTypes.Message:
-        kwargs.update(Utils.RemoveKeys(locals(), 'self', 'kwargs'))
-        
-        response = None
-        if isinstance(kwargs['photo'], str):
-            response = await self._bot._RequestPost(
-                'sendPhoto',
-                MethodForms.SendPhoto(**kwargs)
-            ) 
-        else: 
-            photo_bytes = kwargs.pop('photo')
-            response = await self._bot._RequestPostData(
-                'sendPhoto',
-                MethodForms.SendPhoto(**kwargs),
-                {
-                    'photo': photo_bytes
-                }
-            ) 
-        return self._ResponseToMessage(response)
-        
+ 
